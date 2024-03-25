5085a4952384d4b924c665a79812bc357637d037
Quynh-Truong <146139597+Quynh-Truong@users.noreply.github.com>
2024-03-25 02:41:01 +0100

    removed the fix, since it did not work

--- CV2/src/App.jsx
+++ CV2/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter basename="cv2">
+      <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<About />} />
